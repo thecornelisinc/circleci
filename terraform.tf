30948a01b4ed2a09210efe2f86a4d2fc9358bd01
thecornelisinc <thecornelisinc@gmail.com>
2024-04-15 20:25:47 -0500

    initial commit

--- terraform.tf
+++ terraform.tf
@@ -10,16 +10,16 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # You can change this to your desired AWS region
+  region           = "us-east-1" # You can change this to your desired AWS region
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name1-12345" # Ensure this name is unique
+  bucket      = "my-unique-bucket-name1-12345" # Ensure this name is unique
   acl    = "private"                      # Defines who can access the bucket, e.g., "private", "public-read", etc.
 
   tags = {
-    Name        = "My S3 Bucket"
+    Name                = "My S3 Bucket"
     Environment = "Dev"
   }
 }
