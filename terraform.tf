a3a3c6a11ed2b7a2bfaaee1fe74438bda6b58683
thecornelisinc <thecornelisinc@gmail.com>
2024-04-22 01:06:10 -0500

    update credentials

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket      = "my-unique-bucket-name1-12345" # Ensure this name is unique
+  bucket      = "my-unique-bucket-name1-new" # Ensure this name is unique
   acl    = "private"                      # Defines who can access the bucket, e.g., "private", "public-read", etc.
 
   tags = {
