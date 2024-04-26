d8b697f5bba65fceb72ee53232b64715bc0ffd7a
thecornelisinc <thecornelisinc@gmail.com>
2024-04-26 12:20:47 -0500

    init

--- terraform.tf
+++ terraform.tf
@@ -15,8 +15,8 @@ provider "aws" {
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket      = "my-unique-bucket-name1-12345" # Ensure this name is unique
-  acl    = "private"                      # Defines who can access the bucket, e.g., "private", "public-read", etc.
+  bucket      = "my-unique-bucket-name1-more" # Ensure this name is unique
+
 
   tags = {
     Name                = "My S3 Bucket"
@@ -24,7 +24,3 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-
-# aws_auth:
-#   aws_access_key_id: $accessid
-#   aws_secret_access_key: $secretkey
